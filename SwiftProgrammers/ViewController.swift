fbb50b3f89dee6f7e56232853757541944b5afcf
Simon <simimueller@hotmail.com>
2020-09-15 16:52:59 +0200

    add name labels

--- SwiftProgrammers/ViewController.swift
+++ SwiftProgrammers/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let data = DataProvider(names: ["Jonas", "Simon", "Velo", "Jonas", "Simon", "Velo"])
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 200, width: 415, height: 50))
+        label.text = "Swift Programmierer"
+        label.textAlignment = .center
+        label.textColor = .magenta
+        label.font = .boldSystemFont(ofSize: 25)
+        self.view.addSubview(label)
+        
+        let numberOfNames = data.memberNames.capacity
+        let grid = 400 / numberOfNames
+        var startY = 400
+        for name in data.memberNames {
+            let label = UILabel(frame: CGRect(x: 0, y: startY, width: 415, height: 50))
+            label.text = name
+            label.textAlignment = .center
+            label.textColor = .magenta
+            label.font = .boldSystemFont(ofSize: 25)
+            startY += grid
+            self.view.addSubview(label)
+        }
     }
 
 
+}
+
+class DataProvider {
+    var memberNames : [String]
+    
+    init(names : [String]) {
+        self.memberNames = names
+    }
 }
 
