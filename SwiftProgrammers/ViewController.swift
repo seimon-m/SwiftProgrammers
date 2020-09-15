a9bfb83c818f6cb64762a7b0398af5c74e1e3ef6
Simon <simimueller@hotmail.com>
2020-09-15 22:30:15 +0200

    add Person class and refactor DataProvider as a singleton

--- SwiftProgrammers/ViewController.swift
+++ SwiftProgrammers/ViewController.swift
@@ -9,19 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let usePersonData = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let data = DataProvider()
+        let dataProvider = DataProvider.sharedInstance
         
         let label = UILabel(frame: CGRect(x: 0, y: 200, width: 415, height: 50))
         label.text = "Swift Programmierer"
         label.textAlignment = .center
         label.textColor = .magenta
-        label.font = .boldSystemFont(ofSize: 25)
+        label.font = .boldSystemFont(ofSize: 35)
         self.view.addSubview(label)
-        
-        for (index, name) in data.memberNames.enumerated() {
-            addLabel(name: name, position: index)
+        if usePersonData {
+            for (index, person) in dataProvider.memberPersons.enumerated() {
+                addLabel(person: person, position: index)
+            }
+        } else {
+            for (index, name) in dataProvider.memberNames.enumerated() {
+                addLabel(name: name, position: index)
+            }
         }
     }
     
@@ -39,7 +46,6 @@ class ViewController: UIViewController {
             color = .black
         }
         
-        
         let label = UILabel(frame: CGRect(x: 0, y: yPosition, width: 415, height: 50))
         label.text = name
         label.textAlignment = .center
@@ -47,15 +53,58 @@ class ViewController: UIViewController {
         label.font = .boldSystemFont(ofSize: 25)
         self.view.addSubview(label)
     }
+    
+    func addLabel(person: Person, position: Int) {
+        let yPosition = position * 50 + 400
+        
+        let labelFirst = UILabel(frame: CGRect(x: 20, y: yPosition, width: 415, height: 50))
+        labelFirst.text = person.firstName
+        labelFirst.textAlignment = .left
+        labelFirst.textColor = .purple
+        labelFirst.font = .boldSystemFont(ofSize: 25)
+        self.view.addSubview(labelFirst)
+        
+        let labelLast = UILabel(frame: CGRect(x: 150, y: yPosition, width: 415, height: 50))
+        labelLast.text = person.lastName
+        labelLast.textAlignment = .left
+        labelLast.textColor = .purple
+        labelLast.font = .boldSystemFont(ofSize: 25)
+        self.view.addSubview(labelLast)
+        
+        let labelPlz = UILabel(frame: CGRect(x: 300, y: yPosition, width: 415, height: 50))
+        labelPlz.text = String(person.plz)
+        labelPlz.textAlignment = .left
+        labelPlz.textColor = .purple
+        labelPlz.font = .boldSystemFont(ofSize: 25)
+        self.view.addSubview(labelPlz)
+    }
 
 
 }
 
 class DataProvider {
+    static let sharedInstance = DataProvider()
     var memberNames : [String]
+    var memberPersons : [Person]
     
     init() {
         self.memberNames = ["Jonas", "Simon", "Jirawat", "Wanda", "Ben", "Ruedi"]
+        self.memberPersons = [Person(firstName: "Jonas", lastName: "Wolter", plz: 6003),
+                              Person(firstName: "Simon", lastName: "Müller", plz: 3250),
+                              Person(firstName: "Jirawat", lastName: "Sae-Tang", plz: 2504),
+                              Person(firstName: "Wanda", lastName: "Lao", plz: 6003),
+                              Person(firstName: "Ruedi", lastName: "Arnold", plz: 0000)]
     }
 }
 
+class Person {
+    var firstName : String
+    var lastName : String
+    var plz : Int
+    
+    init(firstName: String, lastName: String, plz: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.plz = plz
+    }
+}
